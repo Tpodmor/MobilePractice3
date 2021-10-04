3756dffa4a59008ba133ff32afbacccd15e747bf
Илья Закатимов <ilya_zakatimov@MacBook-Pro-Ila.local>
2021-10-04 23:23:34 +0300

    Initial commit

---
+++ internetapp/src/main/java/ru/mirea/petukhov/internetapp/MainActivity2.java
@@ -0,0 +1,43 @@
+package ru.mirea.petukhov.internetapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import ru.mirea.petukhov.internetapp.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        Bundle arguments = getIntent().getExtras();
+        TextView textView = (TextView)findViewById(R.id.timeTW);
+        textView.setText(arguments.get("Message").toString());
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
